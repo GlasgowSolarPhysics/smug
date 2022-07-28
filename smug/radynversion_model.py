549bc14449f4ae686261d0f9ef5f94556d9d405f
cmo <c.osborne.1@research.gla.ac.uk>
2022-07-28 17:48:52 +0100

    Added typehints and more docs to radynversion_model

--- smug/radynversion_model.py
+++ smug/radynversion_model.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import FrEIA.framework as Ff
 import FrEIA.modules as Fm
 import numpy as np
@@ -6,19 +8,39 @@ from torch import nn
 
 
 class F_fully_connected_leaky(nn.Module):
-    """Fully connected tranformation, not reversible, but used in the RNVP
-    blocks in the Radynversion model."""
+    """Fully connected transformation, not reversible, but used in the RNVP
+    blocks in the Radynversion model.
+    This is a standard five-layer feed-forward fully connected network, with
+    leaky ReLU's after the first three layers, and a ReLU after the fourth.
+
+    Parameters
+    ----------
+    size_in : int
+        The size of the network input layer.
+    size_out : int
+        The size of the network output layer.
+    internal_size : int, optional
+        The size of the network's three hidden layers (Default: `2 * size_out`).
+    dropout : float, optional
+        The dropout fraction to use during training. (Default: 0.0)
+    batch_norm : bool, optional
+        Whether to apply batch normalisation after the fully connected layers
+        (Default: False).
+    leaky_slope : float, optional
+        The slope to use in the leaky ReLU activations (after the first 3 FC
+        layers).
+    """
 
     def __init__(
         self,
-        size_in,
-        size_out,
-        internal_size=None,
-        dropout=0.0,
-        batch_norm=False,
-        leaky_slope=0.01,
+        size_in: int,
+        size_out: int,
+        internal_size: Optional[int] = None,
+        dropout: float = 0.0,
+        batch_norm: bool = False,
+        leaky_slope: float = 0.01,
     ):
-        super(F_fully_connected_leaky, self).__init__()
+        super().__init__()
         if not internal_size:
             internal_size = 2 * size_out
 
@@ -46,7 +68,7 @@ class F_fully_connected_leaky(nn.Module):
             self.bn2b.weight.data.fill_(1)
         self.batch_norm = batch_norm
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         out = self.fc1(x)
         if self.batch_norm:
             out = self.bn1(out)
@@ -70,18 +92,66 @@ class F_fully_connected_leaky(nn.Module):
 
 
 class RadynversionModel(nn.Module):
-    """RADYNVERSION Invertible Neural Network."""
+    """RADYNVERSION Invertible Neural Network.
+    Constructs the model object to the requisite dimensions using building
+    blocks from the FrEIA package.
+
+    Forward process: atmosphere -> line profiles x latent space
+    Backward process: line profiles x latent space -> atmosphere.
+
+    Parameters
+    ----------
+    in_out_size : int
+        The size of the input and output layers.
+    num_lines : int
+        The number of spectral lines used in the model (e.g. 2 for the standard
+        Radynversion).
+    line_profile_size : int
+        The number of points across each spectral line (assumed the same for
+        all).
+    atmos_size : int
+        The number of points in the atmospheric stratification.
+    num_atmos_params : int
+        The number of stratified atmospheric parameters.
+    latent_size : int
+        The size of the latent space associated with associated with information
+        lost in the forward process.
+    version : str or None
+        A version identifier associated with the current model.
+
+    Attributes
+    ----------
+    model : FrEIA.SequenceINN
+        The model describing the INN.
+    in_out_size : int
+        The size of the input and output layers.
+    num_lines : int
+        The number of spectral lines used in the model (e.g. 2 for the standard
+        Radynversion).
+    line_profile_size : int
+        The number of points across each spectral line (assumed the same for
+        all).
+    atmos_size : int
+        The number of points in the atmospheric stratification.
+    num_atmos_params : int
+        The number of stratified atmospheric parameters.
+    latent_size : int
+        The size of the latent space associated with associated with information
+        lost in the forward process.
+    version : str or None
+        A version identifier associated with the current model.
+    """
 
     def __init__(
         self,
-        in_out_size,
-        num_lines,
-        line_profile_size,
-        atmos_size,
-        num_atmos_params,
-        latent_size,
-        num_inv_layers=5,
-        version=None,
+        in_out_size: int,
+        num_lines: int,
+        line_profile_size: int,
+        atmos_size: int,
+        num_atmos_params: int,
+        latent_size: int,
+        num_inv_layers: int = 5,
+        version: Optional[str] = None,
     ):
         super().__init__()
         self.size = in_out_size
@@ -125,6 +195,9 @@ pretrained_kwargs = {
         "latent_size": 150,
     },
 }
+"""dict of str to dict of str: kwargs needed to instantiate the standard
+pre-trained versioned Radynversion models.
+"""
 
 original_z = np.array(
     [
@@ -181,6 +254,7 @@ original_z = np.array(
     ],
     dtype=np.float32,
 )
+"""array of float32: altitude grid used in the original Radynversion models"""
 
 model_params = {
     "1.0.1": {
@@ -196,14 +270,19 @@ model_params = {
         "z_stratification": original_z,
     },
 }
+"""dict of str to dict: additional parameters needed by the RadynversionAdapter
+for each pre-trained versioned Radynversion model."""
 
 model_urls = {
     "1.0.1": "https://www.astro.gla.ac.uk/users/osborne/Radynversion_1.0.1.pth.tar",
     "1.1.1": "https://www.astro.gla.ac.uk/users/osborne/Radynversion_1.1.1.pth.tar",
 }
+"""dict of str to str: urls to the weights for the pre-trained versioned Radynversion models"""
 
 
-def pretrained_radynversion(version="1.1.1", map_location=None):
+def pretrained_radynversion(
+    version: str = "1.1.1", map_location: Optional[torch.device] = None
+) -> RadynversionModel:
     """Load a pretrained RADYNVERSION model. The weights will be be cached as
     described by `torch.hub`. See their documentation for details.
 
